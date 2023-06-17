92cb0e3f8fe87d587925b41bb8c951e3cafff823
barbarosozkaya <136834730+barbarosozkaya@users.noreply.github.com>
2023-06-17 13:42:32 +0200

    CheckersGame.java

--- CheckersG/src/main/java/com/example/checkersg/CheckersGame.java
+++ CheckersG/src/main/java/com/example/checkersg/CheckersGame.java
@@ -39,7 +39,6 @@ public class CheckersGame extends Application {
         pieces = new Piece[BOARD_SIZE][BOARD_SIZE];
         highlights = new Rectangle[BOARD_SIZE][BOARD_SIZE];
 
-        // CREATING THE BOARD
         for (int col = 0; col < BOARD_SIZE; col++) {
             for (int row = 0; row < BOARD_SIZE; row++) {
                 Rectangle square = new Rectangle(SQUARE_SIZE, SQUARE_SIZE);
@@ -50,7 +49,6 @@ public class CheckersGame extends Application {
                 squares[col][row] = square;
                 board.getChildren().add(square);
 
-                // INITIAL POSITIONS OF PIECES
                 if ((col + row) % 2 != 0 && row < 3) {
                     Piece piece = new Piece(col * SQUARE_SIZE + SQUARE_SIZE / 2, row * SQUARE_SIZE + SQUARE_SIZE / 2, true);
                     pieces[col][row] = piece;
@@ -63,7 +61,6 @@ public class CheckersGame extends Application {
             }
         }
 
-        // CLICKING ACTIONS ON PIECES
         board.setOnMouseClicked(e -> handleBoardClick(e.getX(), e.getY()));
 
         Scene scene = new Scene(board, BOARD_SIZE * SQUARE_SIZE, BOARD_SIZE * SQUARE_SIZE);
@@ -71,7 +68,6 @@ public class CheckersGame extends Application {
         primaryStage.setResizable(false);
         primaryStage.show();
 
-        // STARTING SETTINGS
         selectedPiece = null;
         whiteTurn = true;
         gameOver = false;
@@ -104,10 +100,6 @@ public class CheckersGame extends Application {
     private void showValidMoves(int col, int row) {
         clearHighlights();
 
-        // Mark valid moves
-        // Burada geçerli hamleleri işaretleme işlemini yapmanız gerekmektedir
-
-        // Örnek olarak, seçili taşın hemen yanındaki boş kareleri işaretleyelim
         if (col > 0 && row > 0 && pieces[col - 1][row - 1] == null) {
             Rectangle highlight = new Rectangle(SQUARE_SIZE, SQUARE_SIZE);
             highlight.setX((col - 1) * SQUARE_SIZE);
@@ -128,10 +120,6 @@ public class CheckersGame extends Application {
     }
 
     private boolean isValidMove(int col, int row) {
-        // Check if the move is valid
-        // Burada geçerli hamle kontrolünü yapmanız gerekmektedir
-
-        // Örnek olarak, seçili taşın yalnızca boş kareye hareket etmesine izin verelim
         return pieces[col][row] == null;
     }
 
@@ -144,7 +132,6 @@ public class CheckersGame extends Application {
         selectedPiece.setCenterX(col * SQUARE_SIZE + SQUARE_SIZE / 2);
         selectedPiece.setCenterY(row * SQUARE_SIZE + SQUARE_SIZE / 2);
 
-        // Reorder the piece on the board
         board.getChildren().remove(selectedPiece);
         board.getChildren().add(selectedPiece);
 
